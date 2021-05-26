192c0741473833c3235ecc78ce520e0f50bb4ec2
ckanerloy <camilakanerloy@gmail.com>
2021-05-26 14:35:25 -0300

    Creacion de usuario

---
+++ src/main/java/Dominio/Parte5/Guardaropa.java
@@ -0,0 +1,9 @@
+package Dominio.Parte5;
+
+import Dominio.Prenda;
+
+import java.util.List;
+
+public class Guardaropa {
+  private List<Prenda> prendas;
+}
---
+++ src/main/java/Dominio/Parte5/Usuario.java
@@ -0,0 +1,7 @@
+package Dominio.Parte5;
+
+import java.util.List;
+
+public class Usuario {
+  private List<Guardaropa> guardaropas;
+}
